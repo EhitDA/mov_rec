d850f2035e42e4ab7f13108b3bf1e355ce4d9dc9
EhitDA <166541860+EhitDA@users.noreply.github.com>
2024-12-17 22:16:09 +0530

    Add files via upload

--- p4.py
+++ p4.py
@@ -1,5 +1,44 @@
 import streamlit as st
 import pandas as pd
+
+@st.cache_data
+def load_movs():
+    movs = pd.read_csv("https://liangfgithub.github.io/MovieData/movies.dat?raw=true", sep="::", header=None, engine="python", encoding="ISO-8859-1")
+    movs[0] = movs[0].apply(lambda x: "m" + str(x))
+    movs.columns = ["Movie ID", "Title", "Genres"]
+    return movs.set_index("Movie ID")
+
+@st.cache_data
+def load_S_mat():
+    return pd.read_csv("S.csv")
+
+
+
+movs = load_movs()
+
+S_mat = load_S_mat()
+
+st.header("Movie Recommender")
+
+
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+mov_id = f"m{st.session_state.counter}"
+mov_url = f"https://liangfgithub.github.io/MovieImages/{st.session_state.counter}.jpg"
+mov_ttl = movs.loc[mov_id, "Title"]
+
+st.image(mov_url, caption=mov_ttl, width=300)
+
+rating = st.feedback("stars")
+
+if rating is not None:
+    st.write(f"You gave a rating of {rating + 1} star(s)!")
+
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
@@ -18,22 +57,8 @@ option = st.selectbox(
 
 'You selected: ', option
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
 
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
-
-st.session_state.counter += 1
 
 st.header(f"This page has run {st.session_state.counter} times.")
 st.button("Run it again")
