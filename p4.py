f9e55f478c83aa944ca546145567624a152d886e
EhitDA <166541860+EhitDA@users.noreply.github.com>
2024-12-17 20:26:37 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+option = st.selectbox(
+    'Which number do you like best?',
+    [1, 2, 3, 4, 5]
+)
+
+'You selected: ', option
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
